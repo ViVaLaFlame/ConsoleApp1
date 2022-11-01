068365eed5631970ce62d536a1acc29657c523cf
ViVaLaFlame <amir.kakharov2001@gmail.com>
2022-11-01 18:22:15 +0300

    Программа, которая "еще пригодится"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Привет,\n мир!");
+﻿string name = "Amir";
+byte age = 21;
+bool petExistance = false;
+double shoeSize = 41.5; 
+
+
+Console.WriteLine("My name " + name);
+Console.WriteLine("I'm  " + age);
+Console.WriteLine("Do I have a pet " + petExistance);
+Console.WriteLine("My shoe size is " + shoeSize);
+
+Console.ReadKey();
