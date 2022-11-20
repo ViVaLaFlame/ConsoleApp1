ad140a39e6ed642578931255ca79cf80588337a8
ViVaLaFlame <amir.kakharov2001@gmail.com>
2022-11-20 16:13:03 +0300

    Двумерный массив

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,11 @@
-﻿int t = 1;
+﻿int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-do
+for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
 {
-    switch (Console.ReadLine())
-    {
-        case "red":
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
+    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+        Console.Write(array[k, i] + " ");
 
-            Console.WriteLine("Your color is red!");
-            break;
-
-        case "green":
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is green!");
-            break;
-
-        case "cyan":
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-            break;
-        default:
-            continue;
-    }
-    t++;
+    Console.WriteLine();
 }
-while (t > 0);
+
+Console.ReadLine();
